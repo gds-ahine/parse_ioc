ce5a1d9e66161f615783de39cd9263548a3433b4
Aaron Hine <aaron.hine@digital.cabinet-office.gov.uk>
2019-11-25 01:23:13 +0000

    Latest script

--- send_threat_feed_ioc_to_splunk.py
+++ send_threat_feed_ioc_to_splunk.py
@@ -1,49 +1,44 @@
 #!/usr/bin/env python3
-""" Script to parse IOC (Indicator Of Compromise) from Open Source Threat Feed websites """
 
 import ipaddress
 import requests
 import json
-#import iocextract
 
-""" Splunk HTTP Event Collector URL and Token """
 hec_endpoint = 'http://127.0.0.1:8088/services/collector'
 hec_token = ''
+
 headers = {"Authorization": 'Splunk ' + hec_token}
 plist = []
 list = []
 file = 'feeds.json'
 
-""" Extract IOCs from a website """
 def parse_ioc_from_website(url):
 	try:
 		response = requests.get(url)
-		#response.encoding = 'utf-8'
 		data = response.text
 		ip_list = data.splitlines()
 		return ip_list
 	except Exception:
                 print(f"URL fetch error: {response.text}")
  	
-""" Check IPv4 address or range Does not parse CIDR """
 def validate_ipaddress(ip):
 	try:
 		range_of_ips = []
-		if not ip.startswith('#'):
-			""" Opted to not parse CIDR as they could be large """
+		if not ip or ip.startswith('#'):
+			pass
+		else:
 			cidr_check = ip.split('/')
 			if len(cidr_check) == 2:
 				pass
-			""" parse IP range e.g. 192.168.0.1-192.168.1.254 """
 			parsed_ip = ip.split('-')
 			if len(parsed_ip) == 2:
 				start_ip = ipaddress.IPv4Address(parsed_ip[0])
 				end_ip = ipaddress.IPv4Address(parsed_ip[1])
 				for ip_int in range(int(start_ip), int(end_ip)):
 					range_of_ips.append(str(ipaddress.ip_address(ipaddress.IPv4Address(ip_int))))
-			""" parse a single address """
 			elif len(parsed_ip) == 1:
 				range_of_ips.append(str(ipaddress.ip_address(parsed_ip[0])))
+				print(parsed_ip)
 			else:
 				range_of_ips.append(str(ipaddress.ip_address(ip)))
 			return range_of_ips
@@ -51,7 +46,6 @@ def validate_ipaddress(ip):
        		print(errorCode)
        		return False
  
-""" Send IOCs to Splunk """
 def send_to_splunk(final_payload):
 	try:
 		r = requests.post(hec_endpoint, final_payload, headers=headers, verify=False)
@@ -68,12 +62,6 @@ def send_to_splunk(final_payload):
  
 def main():
 	try:
-		""" testing local files """
-		"""
-                with open(file,"r") as fp:
-                        list = [line.strip() for line in fp.readlines()]
-                fp.close()
-                """
 
 		with open(file, 'r') as fn:
        			 feed_data=fn.read()
@@ -91,13 +79,10 @@ def main():
 					validated_ip = validate_ipaddress(ip)
 					if validated_ip is not None:
 						for ioc in validated_ip:
-							""" build JSON payload to send to Splunk """
 							payload = '{"sourcetype": "_json", "event": {"ip": "' + ioc + '","feed":"' + tf + '"}}'
 							plist.append(payload)
-				""" send single JSON payload to Splunk """
 				final_payload = ''.join(plist)
 				send_to_splunk(final_payload)
-			""" parse using iocextract for these IOCs """
 			elif tf_type == 'domain':
 				pass
 			elif tf_type == 'url':
