969f2dbd65791da918d2a868290d7857a03cf4fd
Aaron Hine <aaron.hine@digital.cabinet-office.gov.uk>
2019-12-14 00:52:33 +0000

    refactoring

--- refactor_send_threat_feed_ioc_to_splunk.py
+++ refactor_send_threat_feed_ioc_to_splunk.py
@@ -3,8 +3,8 @@ import ipaddress
 import json
 import re
 import requests
-import logging
-import boto3
+#import iocextract
+#import boto3
 
 warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 hec_endpoint = 'http://127.0.0.1:8088/services/collector'
@@ -13,67 +13,114 @@ indicator_list = []
 list = []
 urlhaus_list = []
 file = 'feeds.json'
-s3_bucket = 'ah-open-source-threat-feeds-test-bucket'
-s3_ip_filename = 'ip.txt'
+s3_bucket = 'cyber-security-open-source-threat-feed-indicators'
+s3_talos_ip_filename = 'cisco_talos.txt'
+s3_tor_ip_filename = 'tor_exit_nodes.txt'
 s3_domain_filename = 'domain.txt'
 s3_url_filename = 'url.txt'
+s3_urlhaus_filename = 'urlhaus.txt'
 event = ''
 context = ''
+old_talos_file = 'ip-blacklist.old'
+new_talos_file = 'ip-blacklist.new'
+old_tor_file = 'tor.old'
+new_tor_file = 'tor.new'
 
+""" Get Splunk HEC token from SSM Parameter Store  """
+#ssm_client = boto3.client('ssm', region_name='eu-west-2')
 
-def update_s3_indicators_file(tf_type, i_list):
-    tmp_ip_list = []
-    tmp_domain_list = []
-    tmp_url_list = []
-    s3 = boto3.resource('s3')
+def get_ssm_parameter(param):
+    ssm_response = ssm_client.get_parameter(
+        Name=str(param),
+        WithDecryption=True
+        )
+    return ssm_response['Parameter']['Value']
+
+def check_dupes(list_to_dedupe):
+    deduped_list = set(); return [dupe_ioc for dupe_ioc in list_to_dedupe if dupe_ioc not in deduped_list and not deduped_list.add(dupe_ioc)]
+    return deduped_list
+
+def update_s3_indicators_file(tf, tf_type, i_list):
+    #s3 = boto3.resource('s3')
     try:
         """ Check to see if we have previously ingested this IP """
-        if tf_type == 'ip':
+        if tf == 'Cisco Talos blacklist' and tf_type == 'ip':
+            with open(old_talos_file, 'r+') as talos_ro_fn:
+                old_talos_ip_list = talos_ro_fn.read().splitlines()
+            talos_ro_fn.close()
+
+            updated_talos_ip_list = [talos_ip_item for talos_ip_item in i_list if talos_ip_item not in old_talos_ip_list]
+            deduped_talos_ip_list = check_dupes(updated_talos_ip_list)
+
+            with open(old_talos_file, 'a+') as talos_append_fn:
+                for new_talos_ip in deduped_talos_ip_list:
+                    talos_append_fn.write(new_talos_ip + '\n')
+            talos_append_fn.close()
+
+            return deduped_talos_ip_list
+        elif tf == 'Tor exit nodes' and tf_type == 'ip':
+            with open(old_tor_file, 'r+') as tor_ro_fn:
+                old_tor_ip_list = tor_ro_fn.read().splitlines()
+            tor_ro_fn.close()
+
+            updated_tor_ip_list = [tor_ip_item for tor_ip_item in i_list if tor_ip_item not in old_tor_ip_list]
+            deduped_tor_ip_list = check_dupes(updated_tor_ip_list)
+
+            with open(old_tor_file, 'a+') as tor_append_fn:
+                for new_tor_ip in deduped_tor_ip_list:
+                    tor_append_fn.write(new_tor_ip + '\n')
+            tor_append_fn.close()
+
+            return deduped_tor_ip_list
+
+
+            """ update s3 file logic goes here """
+
+            #updated_ip_list = [ip_item for ip_item in i_list if ip_item not in ip_list]
+
+            """
             s3.meta.client.download_file(s3_bucket, s3_ip_filename, '/tmp/' + s3_ip_filename)
 
-            """ Download previously ingested IP file from S3 """
             with open('/tmp/' + s3_ip_filename, 'r+') as ipfile_ro:
                 for ip_line in ipfile_ro:
                     previous_ip = ip_line.strip('\n')
                     tmp_ip_list.append(previous_ip)
             ipfile_ro.close()
 
-            """ Get a list of IPs not already ingested """
+
             updated_ip_list = [ip_item for ip_item in i_list if ip_item not in tmp_ip_list]
 
-            """ Update the S3 IP file """
+
             with open('/tmp/' + s3_ip_filename, 'a+') as ipfile_append:
                 for new_ip in updated_ip_list:
                     ipfile_append.write(new_ip + '\n')
             ipfile_append.close()
 
-            """ Upload the updated IP file to S3 """
+
             s3.meta.client.upload_file('/tmp/' + s3_ip_filename, s3_bucket, s3_ip_filename)
 
-            """ Validate new IPs """
             validated_ip_addresses = validate_ipaddress(updated_ip_list)
             return validated_ip_addresses
 
         elif tf_type == 'domain':
             s3.meta.client.download_file(s3_bucket, s3_domain_filename, '/tmp/' + s3_domain_filename)
 
-            """ Download previously ingested domain file from S3 """
             with open('/tmp/' + s3_domain_filename, 'r+') as domainfile_ro:
                 for domain_line in domainfile_ro:
                     previous_domain = domain_line.strip('\n')
                     tmp_domain_list.append(previous_domain)
             domainfile_ro.close()
 
-            """ Get a list of domains not already ingested """
+
             updated_domain_list = [domain_item for domain_item in i_list if domain_item not in tmp_domain_list]
 
-            """ Update the S3 domain file """
+
             with open('/tmp/' + s3_domain_filename, 'a+') as domainfile_append:
                 for new_domain in updated_domain_list:
                     domainfile_append.write(new_domain + '\n')
             domainfile_append.close()
 
-            """ Upload the updated domain file to S3 """
+
             s3.meta.client.upload_file('/tmp/' + s3_domain_filename, s3_bucket, s3_domain_filename)
 
             return updated_domain_list
@@ -81,38 +128,79 @@ def update_s3_indicators_file(tf_type, i_list):
         elif tf_type == 'url':
             s3.meta.client.download_file(s3_bucket, s3_url_filename, '/tmp/' + s3_url_filename)
 
-            """ Download previously ingested URL file from S3 """
+
             with open('/tmp/' + s3_url_filename, 'r+') as urlfile_ro:
                 for url_line in urlfile_ro:
-                    #print(url_line)
                     previous_url = url_line.strip('\n')
                     tmp_url_list.append(previous_url)
             urlfile_ro.close()
 
-            #print(tmp_url_list)
 
-            """ Get a list of URLs not already ingested """
             updated_url_list = [url_item for url_item in i_list if url_item not in tmp_url_list]
 
-            #print(updated_url_list)
 
-            """ Update the S3 URL file """
             with open('/tmp/' + s3_url_filename, 'a+') as urlfile_append:
                 for new_url in updated_url_list:
                     urlfile_append.write(new_url + '\n')
             urlfile_append.close()
 
-            """ Upload the updated URL file to S3 """
+
             s3.meta.client.upload_file('/tmp/' + s3_url_filename, s3_bucket, s3_url_filename)
 
             return updated_url_list
-        else:
-            pass
+
+        elif tf_type == 'URLHaus':
+            s3.meta.client.download_file(s3_bucket, s3_urlhaus_filename, '/tmp/' + s3_urlhaus_filename)
+
+
+            with open('/tmp/' + s3_urlhaus_filename, 'r+') as urlhausfile_ro:
+                for urlhaus_line in urlhausfile_ro:
+                    previous_urlhaus = urlhaus_line.strip('\n')
+                    tmp_urlhaus_list.append(previous_urlhaus)
+            urlhausfile_ro.close()
+
+
+
+            updated_urlhaus_list = [urlhaus_item for urlhaus_item in i_list if urlhaus_item not in tmp_urlhaus_list]
+
+
+            with open('/tmp/' + s3_urlhaus_filename, 'a+') as urlhausfile_append:
+                for new_urlhaus in updated_urlhaus_list:
+                    urlhausfile_append.write(new_urlhaus + '\n')
+            urlhausfile_append.close()
+
+
+            s3.meta.client.upload_file('/tmp/' + s3_urlhaus_filename, s3_bucket, s3_urlhaus_filename)
+
+            return updated_urlhaus_list
+            """
     except:
         print(f"Unable to update indicators file stored in S3")
 
+def parse_ioc(feed_list, tf, tf_type):
+    feed_ioc_list = [i for i in feed_list if '#' not in i]
+    if tf_type == 'ip':
+        validated_ip = validate_ipaddress(feed_ioc_list)
+        updated_ip_list = update_s3_indicators_file(tf, tf_type, validated_ip)
 
-def parse_ioc_from_website(url):
+        for parsed_ip in updated_ip_list:
+            build_payload(parsed_ip, tf, tf_type)
+        final_ip_payload = ''.join(indicator_list)
+
+        return final_ip_payload
+    elif tf_type == url:
+        updated_url_list = update_s3_indicators_file(tf, tf_type, feed_list)
+
+        for parsed_url in updated_url_list:
+            build_payload(parsed_url, tf, tf_type)
+        final_url_payload = ''.join(indicator_list)
+
+        return final_url_payload
+    else:
+        pass
+
+
+def download_ioc_from_feed(url):
     try:
         response = requests.get(url)
         data = response.text
@@ -144,15 +232,6 @@ def validate_ipaddress(ip):
         return False
 
 
-def parse_alienvault_reputation_data(indicators):
-    av_ioc_list = []
-    for otx_ip in indicators:
-        av_ioc = otx_ip.split('#')
-        av_ioc_list.append(av_ioc[0])
-    parsed_av_list = validate_ipaddress(av_ioc_list)
-    return parsed_av_list
-
-
 """ build IOC payload for Splunk """
 def build_payload(ioc, tf, tf_type):
     payload = '{"sourcetype": "_json", "event": {"feed": "' + tf + '","indicator_type": "' + tf_type + '","indicator":"' + ioc + '"}}'
@@ -161,9 +240,11 @@ def build_payload(ioc, tf, tf_type):
 
 
 """ build URLHaus payload for Splunk """
-def build_urlhaus_payload(tf, tf_type, dateadded, url, url_status, threat, tags, urlhaus_url):
-    payload = '{"sourcetype": "_json", "event": {"feed": "' + tf + '","indicator_type": "' + tf_type + '","indicator": ' + url + ',"status": ' + url_status + ',"threat": ' + threat + ',"tags": ' + tags + ',"date_added": ' + dateadded + ',"urlhaus_url": ' + urlhaus_url + '}}'
-    indicator_list.append(payload)
+def build_urlhaus_payload(tf, tf_type, updated_urlhaus_indicators):
+    for urlhaus_ioc in updated_urlhaus_indicators:
+        urlhaus_ioc = re.split(r',(?=")', urlhaus_ioc)
+        payload = '{"sourcetype": "_json", "event": {"feed": "' + tf + '","indicator_type": "' + tf_type + '","indicator": ' + urlhaus_ioc[2] + ',"status": ' + urlhaus_ioc[3] + ', "threat": ' + urlhaus_ioc[4] + ',"tags": ' + urlhaus_ioc[5] + ',"date_added": ' + urlhaus_ioc[1] + ',"urlhaus_url": ' + urlhaus_ioc[6] + '}}'
+        indicator_list.append(payload)
     return indicator_list
 
 
@@ -186,19 +267,24 @@ def send_to_splunk(final_payload, tf, splunk_hec_token):
 def lambda_handler(event, context):
     try:
         #splunk_hec_token = get_ssm_parameter(ssm_param)
+
         with open(file, 'r') as fn:
             feed_data = fn.read()
 
         threat_feeds = json.loads(feed_data)
 
         for threat_feed in threat_feeds['feeds']:
+            list = []
             tf = threat_feed['feed_name']
             tf_type = threat_feed['feed_type']
             tf_url = threat_feed['feed_url']
-            list = parse_ioc_from_website(tf_url)
-            i_list = [i for i in list if '#' not in i]
+
+            #list = parse_ioc_from_website(tf_url)
+            #i_list = [i for i in list if '#' not in i]
 
             if tf == 'URLHaus':
+                parsed_urlhaus_list = []
+                list = download_ioc_from_feed(tf_url)
                 for line in list:
                     if not line or not line.startswith('#'):
                         ioc = line.split('\n')
@@ -206,24 +292,32 @@ def lambda_handler(event, context):
                         urlhaus_list.append(ioc)
                         for id in ioc:
                             urlhaus_ioc = re.split(r',(?=")', id)
-                            build_urlhaus_payload(tf, tf_type, urlhaus_ioc[1], urlhaus_ioc[2], urlhaus_ioc[3], urlhaus_ioc[4], urlhaus_ioc[5], urlhaus_ioc[6])
-                final_payload = ''.join(indicator_list)
-                send_to_splunk(final_payload, tf, splunk_hec_token)
-            elif tf == 'AlienVault OTX Reputation data':
-                parsed_alienvault_iocs = parse_alienvault_reputation_data(list)
-                for av_indicator in parsed_alienvault_iocs:
-                    build_payload(av_indicator, tf, tf_type)
-                final_payload = ''.join(indicator_list)
-                send_to_splunk(final_payload, tf, splunk_hec_token)
-            elif tf_type == 'ip':
-                validated_ip_list = update_s3_indicators_file(tf_type, i_list)
-                for parsed_ioc_ip in validated_ip_list:
-                    build_payload(parsed_ioc_ip, tf, tf_type)
-                final_payload = ''.join(indicator_list)
+                            parsed_urlhaus_list.append(id)
+
+                updated_urlhaus_indicators = update_s3_indicators_file(tf, parsed_urlhaus_list)
+                new_uh_iocs = build_urlhaus_payload(tf, tf_type, updated_urlhaus_indicators)
+                final_payload = ''.join(new_uh_iocs)
                 if not final_payload:
-                    print(f"no new IPs to send to Splunk")
+                    print(f"no new URLHaus indicators to send to Splunk")
                 else:
                     send_to_splunk(final_payload, tf, splunk_hec_token)
+            elif tf == 'Cisco Talos blacklist' and tf_type == 'ip':
+                talos_feed_list = download_ioc_from_feed(tf_url)
+                final_talos_payload = parse_ioc(talos_feed_list, tf, tf_type)
+
+                if not final_talos_payload:
+                    print(f"No new Cisco Talos indicators to send to Splunk")
+                else:
+                    send_to_splunk(final_talos_payload, tf, splunk_hec_token)
+            elif tf == 'Tor exit nodes' and tf_type == 'ip':
+                tor_feed_list = download_ioc_from_feed(tf_url)
+                final_tor_payload = parse_ioc(tor_feed_list, tf, tf_type)
+                print(final_tor_payload)
+
+                if not final_tor_payload:
+                    print(f"No new Tor exit node indicators to send to Splunk")
+                else:
+                    send_to_splunk(final_tor_payload, tf, splunk_hec_token)
             elif tf_type == 'domain':
                 for domain_name in i_list:
                     extracted_domain = re.match(r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}', domain_name)
@@ -234,15 +328,14 @@ def lambda_handler(event, context):
                 else:
                     send_to_splunk(final_payload, tf, splunk_hec_token)
             elif tf_type == 'url':
-                urls_to_send = update_s3_indicators_file(tf_type, i_list)
-                #for url in i_list:
-                for url in urls_to_send:
-                    build_payload(url, tf, tf_type)
-                final_payload = ''.join(indicator_list)
-                if not final_payload:
-                    print(f"no new URLs to send to Splunk")
+                url_feed_list = download_ioc_from_feed(tf_url)
+                final_url_payload = parse_ioc(url_feed_list, tf, tf_type)
+                print(final_url_payload)
+
+                if not final_url_payload:
+                    print(f"No URL indicators to send to Splunk")
                 else:
-                    send_to_splunk(final_payload, tf, splunk_hec_token)
+                    send_to_splunk(final_url_payload, tf, splunk_hec_token)
             elif tf_type == 'hash':
                 for hash in i_list:
                     if not hash or hash.startswith('#'):
@@ -272,7 +365,6 @@ def lambda_handler(event, context):
     except Exception:
         print(f"Something went wrong - check feeds file is valid JSON")
 
+
 if __name__ == "__main__":
     lambda_handler(event, context)
-    #tf_type = 'ip'
-    #update_s3(tf_type)
